40eccd1ffd89504eff6b66050ca1af697f04159b
abhishek12prusty <soumyakantaprusty24@gmail.com>
2023-06-21 22:55:29 +0530

    third commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "852a13fe10da905e44dcc3017597cc97",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
